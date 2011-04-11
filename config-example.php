e3b60923027365aab96406db3b62acd3be0d41e4
Joar Wandborg <git@wandborg.com>
2011-04-11 11:51:58 +0200

    Corrected error in config-example. You can't use mathematical operators in  declarations.
    Signed-off-by: Joar Wandborg <git@wandborg.com>

--- config-example.php
+++ config-example.php
@@ -4,5 +4,5 @@ class config {
 	const useCloudFiles = true; // Right now, there is no other option (v0.1)
 	const cloudFilesUser = ''; // Rackspace Cloud Files user
 	const cloudFilesKey = ''; // Rackspace Cloud files API key
-	const cloudFilesApplicationExecutedImageTTL = 60 * 60 * 24 * 7; // Image cache TTL, in seconds
+	const cloudFilesApplicationExecutedImageTTL = 604800; // Image cache TTL, in seconds, currently set to a week. (60 * 60 * 24 * 7)
 }
